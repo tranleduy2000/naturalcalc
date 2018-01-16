d992f06a155b125b582d86405f6ffbab73c7b11d
Duy <tranleduy1233@gmail.com>
2018-01-16 22:54:54 +0700

    change color

--- app/src/main/java/com/duy/natural/calc/calculator/settings/SettingActivity.java
+++ app/src/main/java/com/duy/natural/calc/calculator/settings/SettingActivity.java
@@ -3,26 +3,25 @@ package com.duy.natural.calc.calculator.settings;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
+import com.duy.common.ads.AdsSupportActivity;
 import com.nstudio.calc.casio.R;
 
 /**
  * Created by Duy on 1/15/2018.
  */
 
-public class SettingActivity extends AppCompatActivity {
+public class SettingActivity extends AdsSupportActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-
-        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        setupToolbar();
         setTitle(R.string.title_activity_settings);
 
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
         transaction.replace(R.id.content, SettingsFragment.newInstance());
         transaction.commit();
     }
+
 }
